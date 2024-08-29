e727f7d38525b993866f0f9169fa899d6a84ef1b
Nyakamba <omwegaenock@gmail.com>
2024-08-29 16:48:23 +0300

    create dashboard

--- ecommerce-admin/app/layout.tsx
+++ ecommerce-admin/app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Modalprovider } from "@/providers/modal-provider";
 
 import "./globals.css";
+import { Toasterprovider } from "@/providers/toast-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <Toasterprovider />
           <Modalprovider />
           {children}
         </body>
