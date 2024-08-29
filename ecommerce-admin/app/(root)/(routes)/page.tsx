49fc7cff4708258edf85720a27e7cc33ca106114
Nyakamba <omwegaenock@gmail.com>
2024-08-29 16:48:40 +0300

    create dashboard

---
+++ ecommerce-admin/app/(root)/(routes)/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useEffect } from "react";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+const SetupPage = () => {
+  const { isOpen, onOpen } = useStoreModal();
+
+  useEffect(() => {
+    if (!isOpen) onOpen();
+  }, [isOpen, onOpen]);
+
+  return null;
+};
+
+export default SetupPage;
