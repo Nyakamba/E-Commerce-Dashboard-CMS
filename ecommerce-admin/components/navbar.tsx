c0eb7e94353f0350301c79befb53b403fa4f3aeb
Nyakamba <omwegaenock@gmail.com>
2024-09-06 09:02:11 +0300

    navbar

--- ecommerce-admin/components/navbar.tsx
+++ ecommerce-admin/components/navbar.tsx
@@ -1,11 +1,26 @@
+import prismadb from "@/lib/prismadb";
 import { UserButton } from "@clerk/nextjs";
 import { MainNav } from "./main-nav";
+import StoreSwitcher from "./store-switcher";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  });
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
-        <div>This will be a store switcher</div>
+        <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center spacx-4">
           <UserButton afterSignOutUrl="/" />
